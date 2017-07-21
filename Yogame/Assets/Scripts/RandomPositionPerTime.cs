ac339480a16ed3f2b9301ca34fbd9f5cc167ef2c
LeoBronza <leobronza@hotmail.com>
2017-07-20 23:25:32 -0300

    the first decision

--- Yogame/Assets/Scripts/RandomPositionPerTime.cs
+++ Yogame/Assets/Scripts/RandomPositionPerTime.cs
@@ -3,22 +3,22 @@
 public class RandomPositionPerTeam{
 	// 0 => FriendTeam
 	// 1 => EnemyTeam
-	public Vector2 getRandomPositionPerTeam(int team, float sizeToRide){
+	public Vector3 getRandomPositionPerTeam(int team, float sizeToRide){
 		float backGroundLocalScaleX = GameObject.FindGameObjectWithTag("BackGround").transform.localScale.x/2;
 		float backGroundLocalScaleY = GameObject.FindGameObjectWithTag("BackGround").transform.localScale.y/2;
 
 		if (team == 0) {
 			if (new RandomEdgePerTeam ().getRandomEdgePerTeam () == 0)
-				return new Vector2 (-backGroundLocalScaleX-sizeToRide,  Random.Range(0, backGroundLocalScaleY));
-			return new Vector2 (Random.Range(-backGroundLocalScaleX, backGroundLocalScaleX), backGroundLocalScaleY+sizeToRide); 
+				return new Vector3 (-backGroundLocalScaleX-sizeToRide, 0 ,Random.Range(0, backGroundLocalScaleY));
+			return new Vector3 (Random.Range(-backGroundLocalScaleX, backGroundLocalScaleX),0, backGroundLocalScaleY+sizeToRide); 
 
 		} else {
 			if (new RandomEdgePerTeam ().getRandomEdgePerTeam () == 0)
-				return new Vector2 (
-					Random.Range (-backGroundLocalScaleX, backGroundLocalScaleX), 
+				return new Vector3 (
+					Random.Range (-backGroundLocalScaleX, backGroundLocalScaleX), 0,
 					-backGroundLocalScaleY-sizeToRide
 				);
-			return new Vector2 (backGroundLocalScaleX+sizeToRide, Random.Range (-backGroundLocalScaleY, 0));
+			return new Vector3 (backGroundLocalScaleX+sizeToRide,0, Random.Range (-backGroundLocalScaleY, 0));
 		}
 			
 	}
---
+++ Yogame/Assets/Scripts/testenavmeshagent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testenavmeshagent : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+
+	void Update () {
+		agent.SetDestination (GameObject.FindGameObjectWithTag ("Chupinga").transform.position);
+	}
+}
