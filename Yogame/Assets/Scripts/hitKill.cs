63cb33f1c2349af95c60afd9440adb299348df5b
kacampagnolli <kacampagnolli@gmail.com>
2017-07-25 22:06:01 -0300

    HitKill

---
+++ Yogame/Assets/Scripts/hitKill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitKill : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) { 
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+
+				ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+				Debug.DrawRay (ray.origin, ray.direction * 20, Color.red);
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+					Debug.Log ("Hit Something");
+					Destroy (hit.transform.gameObject);
+					Debug.Log (hit.transform);
+				}	
+			}
+		}	
+	}
+}
---
+++ Yogame/Assets/Scripts/killMonster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class touchTest : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) { 
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+
+				ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+				Debug.DrawRay (ray.origin, ray.direction * 20, Color.red);
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+					Debug.Log ("Hit Something");
+					Destroy (hit.transform.gameObject);
+					Debug.Log (hit.transform);
+				}	
+			}
+		}	
+	}
+}
