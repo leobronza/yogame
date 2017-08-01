361c7b87dbac657826a4241fae3d97f6e55dbfc0
kacampagnolli <kacampagnolli@gmail.com>
2017-08-01 00:06:20 -0300

    add Score

--- Yogame/Assets/Scripts/hitKill.cs
+++ Yogame/Assets/Scripts/hitKill.cs
@@ -7,6 +7,8 @@ public class hitKill : MonoBehaviour {
 	Ray ray;
 	RaycastHit hit;
 	// Use this for initialization
+	public int score;
+	public int points = 1;
 	void Start () {
 
 	}
@@ -16,10 +18,11 @@ public class hitKill : MonoBehaviour {
 		if (Input.touchCount > 0) { 
 			if (Input.GetTouch (0).phase == TouchPhase.Began) {
 					ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
-					//Debug.DrawRay (ray.origin, ray.direction * 20, Color.red);
+					//Debug. Ray (ray.origin, ray.direction * 20, Color.red);
 				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
 					//Debug.Log ("Hit Something");
 					Destroy (hit.transform.gameObject);
+					score += points;
 					//Debug.Log (hit.transform);
 				}	
 			}
@@ -29,7 +32,16 @@ public class hitKill : MonoBehaviour {
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
 				Destroy (hit.transform.gameObject);
+				score += points;
 			}
 		}
+	}
+		
+	void OnGUI(){
+		GUIStyle centeredTextStyle = new  GUIStyle ("label");
+		centeredTextStyle.alignment = TextAnchor.MiddleCenter;
+		centeredTextStyle.fontSize = 40;
+		GUI.Label(new Rect (0,0,Screen.width,30),score.ToString(),centeredTextStyle);
+
 	}
 }
