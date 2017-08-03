f04dd63e63c4d767d179281ce103781ae372e2bf
LeoBronza <leobronza@hotmail.com>
2017-08-02 21:55:52 -0300

    individual respawn

---
+++ Yogame/Assets/Scripts/EnemiesRespawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesRespawn : MonoBehaviour {
+
+	public List<GameObject> enemies;
+	public GameObject enemy;
+	int amountEnemies = 25;
+
+	void Start () {
+		enemies = new List<GameObject>();
+		for(int i = 0 ; i < amountEnemies ;i++){
+			enemies.Add(Instantiate(enemy, new RandomPosition().getRandomPosition(0F, 0.01666667F, 0F), Quaternion.identity));
+		}
+	}
+
+	void Update () {
+		for (int i = 0; i < amountEnemies; i++) {
+			if (i >= enemies.Count) {
+				enemies.Add (Instantiate (enemy, new RandomPosition ().getRandomPosition (0F, 0.01666667F, 0F), Quaternion.identity));
+			} 
+			else if (enemies [i] == null) {
+				enemies [i] = Instantiate (enemy, new RandomPosition ().getRandomPosition (0F, 0.01666667F, 0F), Quaternion.identity);
+				for (int j = 0; j < amountEnemies; j++) {
+					if (j != i) {
+						enemies [j].GetComponent<MinionMoviment> ().minionDeadSignal ();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Yogame/Assets/Scripts/MinionMoviment.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMoviment : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent agent;
+	private UnityEngine.AI.NavMeshObstacle obstacle;
+	private GameObject target; 
+	private bool attacking = false;
+	private GameObject[] targets;
+
+	void Awake () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle> ();
+		target = GameObject.FindGameObjectWithTag ("Chupinga");
+		updateTarget ();
+	
+	}
+
+	void Update () {
+			if (agent.enabled == true && agent.pathPending == false && agent.remainingDistance <= 1.3f &&  agent.remainingDistance != 0){
+				agent.enabled = false;
+				obstacle.enabled = true;
+				obstacle.carving = true;
+				if (Vector3.Distance (this.transform.position, target.transform.position) <= 1.3f) {
+					attacking = true;
+					damage ();//Time
+				}
+			}
+			if (target == null) {
+				updateTarget ();
+				attacking = false;
+			}
+
+	}
+
+	void updateTarget(){
+		//caca novo alvo
+		//float lessDistance = 0f;
+		//if (this.tag == "Enemy") {
+		//	targets = GameObject.FindGameObjectsWithTag ("Enemy");
+		//} else {
+		//	targets = GameObject.FindGameObjectsWithTag ("Allies");
+		//}
+		//for (int i = 0; i < targets.Length; i++) {
+		//	if (target == null) {
+		//		target = targets [i];
+		//		lessDistance = Vector3.Distance (this.transform.position, targets [i].transform.position);
+		//	} else if(lessDistance > Vector3.Distance (this.transform.position, targets [i].transform.position)){
+		//		target = targets [i];
+		//		lessDistance = Vector3.Distance (this.transform.position, targets [i].transform.position);
+		//	}
+		//}
+		agent.SetDestination (target.transform.position);
+	}
+
+	public void minionDeadSignal(){
+		if(obstacle.enabled == true && attacking == false){
+			obstacle.enabled = false;
+			agent.enabled = true;
+			updateTarget ();
+		}
+	}
+
+	void damage(){
+		//vida abaixa
+	}
+
+}
